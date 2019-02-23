74d6f18047ef5e5c65491e4943c582145ad41268
Joe Vasquez <josevwebpro@gmail.com>
2019-02-22 22:37:36 -0700

    initial commit

---
+++ optional.c
@@ -0,0 +1,28 @@
+/*************************
+section 5.(optional)
+*************************/
+
+#include <stdio.h>
+
+int main() {
+
+    //typedef struct student_record student_t;
+    typedef struct {
+        char first_name[10];
+        char last_name[10];
+        char nine_hundred[10];
+        int year_first_enrolled;
+    } student_t, *student_p;
+
+    student_t lm245 = {"logan", "monaghan", "900987321", 2015};
+    student_t *student_handle = &lm245;
+
+    printf("--------------- section i. and ii. ---------------\n");
+    printf("%s-%s-%s-%d\n", student_handle->first_name, 
+                            student_handle->last_name, 
+                            student_handle->nine_hundred, 
+                            student_handle->year_first_enrolled);
+
+    return 0;
+   
+}
