5acfc7d8729ff7eab7a5ebde0fad618c37ddc730
Joe Vasquez <josevwebpro@gmail.com>
2019-02-27 21:56:27 -0700

    finish section 5. (optional)

--- optional.c
+++ optional.c
@@ -15,13 +15,14 @@ int main() {
     } student_t, *student_p;
 
     student_t lm245 = {"logan", "monaghan", "900987321", 2015};
-    student_t *student_handle = &lm245;
+    student_t* student_handle = &lm245;
 
     printf("--------------- section i. and ii. ---------------\n");
     printf("%s-%s-%s-%d\n", student_handle->first_name, 
                             student_handle->last_name, 
                             student_handle->nine_hundred, 
                             student_handle->year_first_enrolled);
+    printf("address of variable that student_handle points to = %p\n", student_handle);
 
     return 0;
    
